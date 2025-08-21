b07a04f7a80749ca6093c17da729f2862d7d1d9f
HARMONY <harmony.etas@maxfameltd.com>
2025-08-21 20:09:25 +0200

    wip

--- src/app/shared/components/network-modal/network-modal.component.ts
+++ src/app/shared/components/network-modal/network-modal.component.ts
@@ -1,33 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { CardItem } from '../../../core/models/modal-card';
 
 @Component({
   selector: 'app-network-modal',
   imports: [CommonModule],
   templateUrl: './network-modal.component.html',
-  styleUrl: './network-modal.component.scss'
+  styleUrl: './network-modal.component.scss',
 })
 export class NetworkModalComponent {
   @Input() backgroundType!: string;
   @Input() item!: string;
+  @Input() cardMargin: string = '-ml-6';
 
   ipAddresses: string[] = [
     '1.2.3.4',
-    '1.2.3.4', 
     '1.2.3.4',
     '1.2.3.4',
     '1.2.3.4',
-    '1.2.3.4'
+    '1.2.3.4',
+    '1.2.3.4',
   ];
 
-  ips: string[] = [
-    '1.2.3.4',
-    '1.2.3.4', 
-  ];
-
-
-
-  
-  
+  ips: string[] = ['1.2.3.4', '1.2.3.4'];
 }
